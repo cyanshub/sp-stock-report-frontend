70bb989a361dbe86d5dc077b6d12eec6b6093743
travel15740 <travel15740@gmail.com>
2025-01-23 00:54:44 +0800

    fix: 優化首頁標題及清單樣式

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import Header from '@/src/components/Header'
+import styled from 'styled-components'
 
 export default function HomePage() {
   const [inputText, setInputText] = useState('')
@@ -18,21 +19,23 @@ export default function HomePage() {
       <Header currentPage="home" />
       <main>
         <section className="container py-16 text-left">
-          <h1 className="title">股票資訊分析</h1>
-          <ul>
-            <li>我們幫你整理了 Yahoo Finance 提供的數據, 讓你可以輕鬆查看即時的交易資訊, 還有過去一年的財務數據</li>
-            <li>根據歷史收盤價, 我們計算了 10 日平均線、50 日平均線, 相對強弱指數 (RSI) 等指標, 更加清楚股價位於高檔還是低檔</li>
-            <li>如果有需要, 你也可以把這些整理好的報表直接下載回去，方便又實用！</li>
-          </ul>
+          <Title>股票資訊分析</Title>
+          <StyledList>
+            <ListItem>我們幫你整理了 Yahoo Finance 提供的數據, 讓你可以輕鬆查看即時的交易資訊, 還有過去一年的財務數據</ListItem>
+            <ListItem>
+              根據歷史收盤價, 我們計算了 10 日平均線、50 日平均線, 相對強弱指數 (RSI) 等指標, 更加清楚股價位於高檔還是低檔
+            </ListItem>
+            <ListItem>如果有需要, 你也可以把這些整理好的報表直接下載回去，方便又實用！</ListItem>
+          </StyledList>
         </section>
 
         <section className="bg-gray-800 py-16">
           <div className="container">
-            <h2 className="title text-center">
+            <Subtitle>
               <label htmlFor="stock-search" className="cursor-pointer">
                 查看股票資料
               </label>
-            </h2>
+            </Subtitle>
             <form onSubmit={handleSubmit} className="max-w-md mx-auto">
               <div className="relative">
                 <input
@@ -60,3 +63,26 @@ export default function HomePage() {
     </div>
   )
 }
+
+const Title = styled.h1`
+  font-size: 1.8rem;
+  margin-bottom: 1.2rem;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  text-align: center;
+  margin-bottom: 1.5rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: disc; /* 保留項目符號 */
+  padding-left: 1.5rem; /* 增加左邊距與符號分開 */
+  margin-top: 1rem;
+  line-height: 1.5;
+`
+
+const ListItem = styled.li`
+  margin-bottom: 0.75rem;
+  font-size: 0.8rem;
+`
