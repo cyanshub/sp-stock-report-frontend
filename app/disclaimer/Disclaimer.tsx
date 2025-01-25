a7506f09e7911c57daa711d15bbb3c5a6164a51f
travel15740 <travel15740@gmail.com>
2025-01-25 23:04:15 +0800

    fix: 微調 li 的 key 屬性

--- app/disclaimer/Disclaimer.tsx
+++ app/disclaimer/Disclaimer.tsx
@@ -7,7 +7,6 @@ export default function Disclaimer() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
       <Header currentPage="others" />
-
       <main className="container mx-auto px-6 py-16">
         <h1 className="text-4xl font-bold mb-8">免責聲明</h1>
         <div className="space-y-6 text-gray-300">
@@ -19,10 +18,10 @@ export default function Disclaimer() {
 
           <p>使用本網站的用戶應知悉：</p>
           <ul className="list-disc list-inside pl-4 space-y-2">
-            <li>股市本質上是無法預測且波動性高的。</li>
-            <li>我們的分析與信號無法保證未來的表現。</li>
-            <li>所有的投資決策應在諮詢合格的財務顧問並進行自行研究後做出。</li>
-            <li>我們不對使用本網站所提供資訊而導致的任何損失負責。</li>
+            <li key="disclaimer-1">股市本質上是無法預測且波動性高的。</li>
+            <li key="disclaimer-2">我們的分析與信號無法保證未來的表現。</li>
+            <li key="disclaimer-3">所有的投資決策應在諮詢合格的財務顧問並進行自行研究後做出。</li>
+            <li key="disclaimer-4">我們不對使用本網站所提供資訊而導致的任何損失負責。</li>
           </ul>
           <p>使用本網站即表示您已閱讀並理解本免責聲明，並同意自行承擔使用本資訊的風險。</p>
           <p>如果您不同意或無法接受本免責聲明，請勿使用本網站或其服務。</p>
